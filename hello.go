f2108d642641c162afd66a163bb350540becd9be
Ekkalak Chimjan <ch.ekkalak@gmail.com>
2017-11-01 19:24:24 +0700

    update hello.go

--- hello.go
+++ hello.go
@@ -3,3 +3,21 @@ package main
 func main(){
 	print("hello world\n")
 }
+/*
+//launch.json
+{
+    // Use IntelliSense to learn about possible attributes.
+    // Hover to view descriptions of existing attributes.
+    // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "name": "Launch file",
+            "type": "go",
+            "request": "launch",
+            "mode": "debug",
+            "program": "${workspaceRoot}/GoVSCode"
+        }
+    ]
+}
+*/
